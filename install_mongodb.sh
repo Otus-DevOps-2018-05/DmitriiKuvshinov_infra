d8512e30a2dd2e8cac83bd9a565a92007e1479e4
Dmitrii Kuvshinov <kuvshinov@embria.com>
2018-07-03 18:38:44 +0300

    MongoDB edited

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 apt-get update
 apt-get install -y mongodb-org
