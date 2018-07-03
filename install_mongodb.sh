16ccd0d789fbcf62db1f24bc066c2c6e8ec6ecf1
Dmitrii Kuvshinov <kuvshinov@embria.com>
2018-07-03 19:22:41 +0300

    updated install SH

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-apt update && apt install -y mongodb-org
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+apt-get update
+apt-get install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
-EOF
+
+
--- install_ruby.sh
+++ install_ruby.sh
@@ -3,6 +3,6 @@ apt update
 apt install -y ruby-full 
 apt install -y ruby-bundler 
 apt install -y build-essential
-EOF
+
 
 
