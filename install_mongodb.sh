9c807c52155db303c5e27635e8459f41918ea8e0
Dmitrii Kuvshinov <kuvshinov@embria.com>
2018-07-03 19:16:25 +0300

    install mongo DB

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-apt-get update
-apt-get install -y mongodb-org
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+apt update && apt install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
 EOF
