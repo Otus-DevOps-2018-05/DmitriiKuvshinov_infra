04e49e0683611b7fb7ca60139f1d3bf0caf1157f
DmitriiKuvshinov <40057838+DmitriiKuvshinov@users.noreply.github.com>
2018-07-13 13:34:39 +0300

    Update startup_puma.sh

--- packer/files/startup_puma.sh
+++ packer/files/startup_puma.sh
@@ -9,5 +9,4 @@ sudo git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 
 sudo echo -e "[Unit] \nDescription=Puma \n[Service]\nExecStart=/usr/local/bin/puma -d\n[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/puma.service
-sudo systemctl start puma.service
 sudo systemctl enable puma.service
