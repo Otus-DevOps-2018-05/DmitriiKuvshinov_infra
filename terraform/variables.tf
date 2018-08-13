abb764096900c2aa2dd6f0eadda9fafb2d53c27e
Dmitrii Kuvshinov <kuvshinov@embria.com>
2018-08-13 18:17:54 +0300

    19

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,12 +8,6 @@ variable region {
 }
 
 variable zone {
-  description = "Zone"
-  default     = "europe-west1-b"
-}
-
-variable public_key_path {
-  description = "Path to the public key used for ssh access"
   description = "Region"
   default     = "us-west1-b"
 }
@@ -30,8 +24,4 @@ variable db_disk_image {
 
 variable public_key_path {
   default = "~/.ssh/appuser.pub"
-}
-
-variable private_key {
- description = "File of private key"
 }
